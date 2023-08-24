c76e61be0446fc279d3853a82b5cb673809ef7d3
michellejanay <mindyourphotosynthesis@gmail.com>
2023-08-24 21:11:31 +0100

    style: profile picture added, icons added, button styling

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState, useEffect } from "react";
+import { BiDownload, BiRightArrowAlt } from "react-icons/bi";
 
 export const Banner = () => {
   const toRotate = ["Web Designer", "Web Developer", "Software Developer"];
@@ -46,21 +47,32 @@ export const Banner = () => {
   };
   return (
     <>
-      <main id="home" className="min-w-full">
-        <span className="tagline">Welcome to my Portfolio</span>
-        <div className="flex flex-col lg:flex-row justify-cetner">
-          <div className="order-2 lg:order-1">
+      <main id="home" className="min-w-full my-11">
+        <h1 className="text-xl my-3">{"Welcome to my Portfolio"}</h1>
+        <div className="flex flex-col lg:flex-row justify-center justify-evenly">
+          <div className="order-2 lg:order-1 mx-3">
             <h1 className="text-[3em]">
               {`Hi, I'm Michelle `}
               <span className="flex flex-wrap text-[.75em]">{text}</span>
             </h1>
-            <button className="btn" onClick={() => console.log("lets connect")}>
-              {" "}
-              Let's connect (arrow)
-            </button>
+            <div className="flex my-4">
+              <button
+                className="flex justify-center content-center border px-6 py-2 rounded-lg"
+                onClick={() => console.log("lets connect")}
+              >
+                {"CV "} <BiDownload size="1.3em" className="mx-2" />
+              </button>
+              <button
+                className="flex justify-center content-center mx-6 px-6 py-2 border rounded-lg"
+                onClick={() => console.log("lets connect")}
+              >
+                {"Let's connect "} <BiRightArrowAlt size="1.5em" />
+              </button>
+            </div>
           </div>
-          <div className="order-1 lg:order-2">
-            <img src="" alt="michelle" />
+          <div className="order-1 lg:order-2 mx-3">
+            <img src="hi.png" alt="michelle" className="relative w-96" />
+            <div className="w-72 h-72 bg bg-white rounded-full absolute mt-[-18.75rem] z-[-1]"></div>
           </div>
         </div>
       </main>
