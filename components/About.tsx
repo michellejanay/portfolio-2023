c3706237f91584a7501b14418cc3a6e098513636
michellejanay <mindyourphotosynthesis@gmail.com>
2023-10-25 18:33:15 +0100

    feat: about me content updated

--- components/About.tsx
+++ components/About.tsx
@@ -4,16 +4,39 @@ export const About = () => {
   return (
     <main className="my-[20] mx-3 lg:w-3/5 lg:m-auto lg:text-center" id="about">
       <p className="py-5">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
-        animi nemo distinctio fugit, neque voluptatibus dolore nisi quam
-        voluptate velit, omnis maxime saepe facere ex. Facere dolorem ad eaque
-        laborum! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis
-        natus, velit voluptates consequuntur dolores labore id voluptatibus ea
-        doloribus, ipsum necessitatibus rerum provident maiores eum maxime error
-        obcaecati nesciunt sed. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Dolor reprehenderit est nam nemo quas, velit fuga
-        ipsum. Hic, eligendi! Officia expedita, voluptatibus optio aut nostrum
-        aliquid voluptate et. Eveniet, non?
+        As someone who has 8+ years of experience in the Hospitality industry,
+        I've had many various service-based roles. I wanted a change in my life,
+        so I decided to retake the Myers-Briggs personality test to see what
+        types of roles I could be suitable for. One that caught my eye was Web
+        Development / Software Engineering. I never knew the two could be so
+        similar.
+      </p>
+    
+      <p className="py-5">
+        Well, not that similar, really. At first, I jumped right in. I joined an
+        intense 24-week Bootcamp, and as embarrassing as this is to say, I had
+        no idea what I was getting myself into. HTML, CSS, great, fantastic I
+        thought, I can do this! Then we got to JavaScript and I thought what (in
+        the console.log('hello world')) is going on!? At the beginning of 2021,
+        I no longer continued on that course. From there, I started
+        self-teaching through Free Code Camp, Udemy, playing games like Grid
+        Attack or solving kata on CodeWars, using any and every cheap/free
+        resource I could find to practice and learn, especially in JavaScript,
+        until in 2022, I was accepted as a Trainee in CodeYourFuture, in March
+        2023 I graduated with a certification in Full Stack Web Development, and
+        in April 2023 I accepted my first contract position as a Software
+        Developer.
+      </p>
+      <p className="py-5">
+        I'm grateful that I didn't stop and I am grateful for the community I
+        found through CodeYourFuture. Because now, two years later, I've
+        rediscovered myself in the best of ways. I've learned how much I enjoy
+        learning new things. I've learned how passionate I am about solving
+        problems and helping others solve problems. I've gotten to know how
+        great it is to be part of a development team, building solutions toward
+        greater goals. I've rediscovered a fire, a drive, a vibrancy, but most
+        importantly, I discovered a new and incredibly gratifying way to express
+        my creativity; through service, solutions, and syntax.
       </p>
     </main>
   );
